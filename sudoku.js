00aa6014543fa5b29bd7546541937a1628a7b2b6
Stefan Caras <stefan.caras@gmail.com>
2023-02-19 19:12:23 +0200

    improvements

--- sudoku.js
+++ sudoku.js
@@ -11,12 +11,15 @@ let board = [
 
 const $ = query => document.querySelector(query)
 const boxes = document.querySelectorAll(".box")
+let solved = []
+board.forEach(el => solved.push([...el]))
 
 const fillBoxes = () => {
   boxes.forEach(el => {
     const array = el.dataset.id.split("")
     if (board[array[0]][array[1]] !== 0) {
       el.textContent = board[array[0]][array[1]]
+      el.style = "color: black"
     } else el.textContent = ""
   })
 }
@@ -27,15 +30,17 @@ $("#table").addEventListener("keydown", (event) => {
   if (event.key.match(/[1-9]/)) {
     event.target.textContent = event.key
     board[array[0]][array[1]] = Number(event.key)
+    if (board[array[0]][array[1]] === solved[array[0]][array[1]]) {
+      event.target.style = "color: green"
+    } else event.target.style = "color: red"
   } else if (event.key === "Backspace" || event.key === "Delete") {
     event.target.textContent = ""
     board[array[0]][array[1]] = 0
   }
-  console.table(board)
 })
 $("#buttons").addEventListener("click", (event) => {
   if (event.target.id === "solve") {
-    solve()
+    solve(board)
     fillBoxes()
   } else if (event.target.id === "clear") {
     board = board.map(el => el.fill(0))
@@ -43,14 +48,14 @@ $("#buttons").addEventListener("click", (event) => {
   }
 })
 
-const solve = () => {
+const solve = (board) => {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       if (board[i][j] === 0) {
         for (let n = 1; n < 10; n++) {
-          if (possible(i, j, n)) {
+          if (possible(i, j, n, board)) {
             board[i][j] = n
-            if (solve()) return true
+            if (solve(board)) return true
             board[i][j] = 0
           }
         }
@@ -61,7 +66,7 @@ const solve = () => {
   return true
 };
 
-const possible = (y, x, n) => {
+const possible = (y, x, n, board) => {
   let x0 = Math.floor(x / 3) * 3
   let y0 = Math.floor(y / 3) * 3
   for (let i = 0; i < 9; i++) {
@@ -77,3 +82,4 @@ const possible = (y, x, n) => {
   return true
 };
 
+solve(solved)
